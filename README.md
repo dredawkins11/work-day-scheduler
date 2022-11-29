3e7dddd1e14e935937081d292cfed58988aaf241
Dre <dredawkins11@gmail.com>
2022-11-28 21:55:41 -0600

    added files

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my module 1 challenge that I have completed. To complete this challenge I was required to create a simple time blocking callender that allows for text to saved and to implement conditional styling. 
+
+This challenge helped me practice and utilize my skills using third-party APIs, such as JQuery and momentJs.
+
+**Deployed Site**
---
+++ index.js
@@ -0,0 +1,40 @@
+// Style timeblocks based on current time
+const TEST_TIME = "2022-11-28T13:59:00.000"
+let currentTime = dayjs(TEST_TIME)
+// let currentTime = dayjs()
+$("#currentDay").text(currentTime.format("MMM DD, YYYY hh:mm"))
+
+$(".time-block").each( (i, element) => {
+    const currentBlock = $(element)
+    const blockTime = dayjs(`${currentTime.year()}-${currentTime.month()}-${currentTime.date()}${$(element).data("time")}`)
+
+    if (blockTime.hour() < currentTime.hour()) currentBlock.addClass("past")
+    if (blockTime.hour() > currentTime.hour()) currentBlock.addClass("future")
+    if (blockTime.hour() == currentTime.hour()) currentBlock.addClass("present")
+
+
+    const currentTextArea = $(currentBlock.children()[1])
+    const time = $(currentBlock.children()[0]).text()
+    const savedData = JSON.parse(localStorage.getItem(time))
+    if (savedData) {
+        currentTextArea.text(savedData.text)
+    }
+})
+// First get all saved time blocks for the day
+// Populate timeblocks with saved data
+
+
+// Add event listeners to save buttons to save data to local storage on click
+$(".saveBtn").click( (e) => {
+    const saveBtnElement = $(e.target)
+    const textAreaElement = saveBtnElement.prev()
+    const inputValue = textAreaElement.val()
+
+    const timeBlockData = {
+        time: dayjs(`${currentTime.year()}-${currentTime.month()}-${currentTime.date()}${saveBtnElement.parent().data("time")}`),
+        index: saveBtnElement.parent().index(),
+        text: inputValue
+    }
+
+    localStorage.setItem(saveBtnElement.prev().prev().text(), JSON.stringify(timeBlockData))
+})
